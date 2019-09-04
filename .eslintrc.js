4a4e43cba63a989750c9ab6a35afe9b34dafcd50
Marcio Monte <marcio.monte@proabakus.com.br>
2019-09-04 15:51:54 -0300

    listagem de prestadores de serviço

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
-    'camelcase': 'off',
-    'no-unused-vars': 'off'
+    camelcase: 'off',
+    'no-unused-vars': 'off',
     // 'no-unused-vars': ['error', { argsIgnorePatterm: 'next' }]
-  }
+  },
 };
--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,8 +1,13 @@
-import User from '../models/User';
+import File from '../models/File';
 
 class FileController {
   async store(req, res) {
-    return res.json(req.file);
+    const { originalname: name, filename: path } = req.file;
+    const file = await File.create({
+      name,
+      path,
+    });
+    return res.json(file);
   }
 }
 
