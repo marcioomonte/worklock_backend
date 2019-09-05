9ba8ba3b043aecfbdbb35ca66fbddf526d56794f
Márcio Monte <marcioomonte@gmail.com>
2019-09-04 23:12:20 -0300

    create de appointments

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
-  // username: 'postgres',
-  username: 'tenanty',
-  password: 'sfkjKGIYG*&$DKJHJKHL',
+  username: 'postgres',
+  // username: 'tenanty',
+  // password: 'sfkjKGIYG*&$DKJHJKHL',
+  password: 'postgres',
   database: 'gobarber',
   define: {
     timestamps: true,
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,4 +20,5 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 export default routes;
