5e71dc85cf4b6fb809047edf974990c3b7ddbd9e
Márcio Monte <marcioomonte@gmail.com>
2019-09-05 00:49:02 -0300

    listagem da agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,4 +23,5 @@ routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 export default routes;
