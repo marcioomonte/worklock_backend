c01f1347e5681ffe36a551cc6ce53efd677d3361
Márcio Monte <marcioomonte@gmail.com>
2019-09-04 23:58:14 -0300

    index de appoitments de usuario

--- src/routes.js
+++ src/routes.js
@@ -21,4 +21,5 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 export default routes;
